4bec67e71c549e74743f8114c2c46d6262c89f98
abhinandan-pandey <abhinandan.pandey@kreeti.com>
2021-10-17 23:29:21 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,102 @@
+const path = require("path");
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
+
+const errorController = require("./controllers/error");
+const User = require("./models/user");
+
+const MONGODB_URI =
+  "mongodb+srv://Abhinandan-Pandey:annupam1997@cluster0-iglwv.mongodb.net/Node-concepts";
+
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
+const csrfProtection = csrf();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  console.log(res.locals.isAuthenticated, res.locals.csrfToken, "iuy");
+  next();
+});
+
+app.use((req, res, next) => {
+  // throw new Error('Sync Dummy');
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      next(new Error(err));
+    });
+});
+app.use((req, res, next) => {
+  console.log(req.session.isLoggedIn, "check");
+  next();
+  console.log(req.session.isLoggedIn, "check end");
+});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.get("/500", errorController.get500);
+
+app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  // res.status(error.httpStatusCode).render(...);
+  // res.redirect('/500');
+  console.log(error, req.body, req.session, "app sess");
+  res.status(500).render("500", {
+    pageTitle: "Error!",
+    path: "/500",
+    isAuthenticated: req.session.isLoggedIn,
+  });
+});
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ routes/admin.js
@@ -0,0 +1,46 @@
+const path = require("path");
+
+const express = require("express");
+const { body } = require("express-validator");
+
+const adminController = require("../controllers/admin");
+const isAuth = require("../middlware/is-auth");
+const fileUpload = require("../util/fileUpload");
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get("/add-product", isAuth, adminController.getAddProduct);
+
+// /admin/products => GET
+router.get("/products", isAuth, adminController.getProducts);
+
+// /admin/add-product => POST
+router.post(
+  "/add-product",
+  fileUpload.single("image"),
+  [
+    body("title").isString().isLength({ min: 3 }).trim(),
+    body("price").isFloat(),
+    body("description").isLength({ min: 5, max: 400 }).trim(),
+  ],
+  isAuth,
+  adminController.postAddProduct
+);
+
+router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
+
+router.post(
+  "/edit-product",
+  [
+    body("title").isString().isLength({ min: 3 }).trim(),
+    body("price").isFloat(),
+    body("description").isLength({ min: 5, max: 400 }).trim(),
+  ],
+  isAuth,
+  adminController.postEditProduct
+);
+
+router.delete("/product/:productId", isAuth, adminController.deleteProduct);
+
+module.exports = router;
